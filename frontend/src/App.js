bcfb44842f1b4d9885903a6c0306b25e91271f47
RahimaKhanam <rahima.khanam45@gmail.com>
2021-09-04 22:57:48 +0530

    pulling changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,35 @@
 import './App.css';
+import { Badge } from 'react-bootstrap';
 import Header from './components/Header/index'
 import Products from './containers/product'
 import CarouselCont from './components/CarouselCont';
-import Cards from './components/cards';
+import Cards from './components/Cards';
 
 
 function App() {
   return (
     <>
       <Header />
-      <CarouselCont/>
-      <Cards/>
-      <Products/>
-      </>
+      <CarouselCont />
+      <div>
+        <h1 style ={{
+          margin: '20px 0 20px 10px'
+        }}>
+          Deals of the day <Badge bg="success">New</Badge>
+        </h1>
+      </div>
+      <Cards />
+
+      <div>
+        <h1 style ={{
+          margin: '20px 0 20px 10px'
+        }}>
+          Top offers  <Badge bg="secondary">now</Badge>
+        </h1>
+      </div>
+
+      <Products />
+    </>
 
   );
 }
